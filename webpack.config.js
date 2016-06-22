6a6e895da2ac6e8cf88659eafb4d6a403de4f44b
Martin Muzatko <martin@happy-css.com>
2016-06-22 09:42:02 +0200

    Setup eslint and sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,16 @@ module.exports = {
     },
     module: {
         preLoaders: [
-            { test: /\.html$/, loader: 'riotjs' }
+            { test: /\.html$/, loader: 'riotjs' },
+            { test: /\.js$/, loader: 'eslint!source-map' },
         ],
         loaders: [
             { test: /\.md$/, loader: 'html!markdown'},
             { test: /\.html$|\.js$/, loader: 'babel', query: { presets: 'es2015-riot' }},
             { test: /\.less$/, loader: 'style!css!less'},
         ]
+    },
+    eslint: {
+        configFile: './.eslintrc'
     }
 }
